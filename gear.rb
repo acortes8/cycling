e318e8ea408bb1053e7ea9222e558fb2501009aa
acortes8 <46253800+acortes8@users.noreply.github.com>
2025-02-20 02:01:12 -0600

    Create new wheel class, and DRY out Gear's methods

--- gear.rb
+++ gear.rb
@@ -1,11 +1,12 @@
-class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+require_relative "wheel"
 
-  def initialize(chainring, cog, rim, tire)
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -13,10 +14,13 @@ class Gear
   end
 
   def gear_inches
-    #tire goes around rim twice for diameter
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(30, 27, 24, 1.25).gear_inches
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
